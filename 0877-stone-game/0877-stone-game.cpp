9da8b25254486501cab2c2eda02662ce55d50171
ranjan-18 <143817387+ranjan-18@users.noreply.github.com>
2026-08-02 08:08:04 +0530

    Time: 22 ms (7.49%), Space: 19.6 MB (27.79%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+ int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == j)
+            return nums[i];
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        int takeLeft = nums[i] - solve(i + 1, j, nums, dp);
+        int takeRight = nums[j] - solve(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(takeLeft, takeRight);
+    }
+    bool stoneGame(vector<int>& piles) {
+         int n = piles.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return solve(0, n - 1, piles, dp) >= 0;
+        
+    }
+};
